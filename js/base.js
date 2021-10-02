60188777dee91a7734392e2fa7bfa828b5968636
alfille <paul.alfille@gmail.com>
2021-10-02 07:43:59 -0600

    Try Service Worker code

--- js/base.js
+++ js/base.js
@@ -1,7 +1,12 @@
-(function( window ) {
-	'use strict';
-
-	if ( location.hostname === 'todomvc.com' ) {
-		var _gaq=[['_setAccount','UA-31081062-1'],['_trackPageview']];(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.src='//www.google-analytics.com/ga.js';s.parentNode.insertBefore(g,s)}(document,'script'));
-	}
-})( window );
+// From: https://developers.google.com/web/fundamentals/primers/service-workers/
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
